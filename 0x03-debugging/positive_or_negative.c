ec35419d4bb8cda1354dd9dbc0f85b4893958661
RodrigueMbiabo <rodriguembiabo@gmail.com>
2022-03-15 17:48:56 -0700

    change positive and negative

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,31 +1,17 @@
 #include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 
-/* betty style doc for function main goes there */
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * positive_or_negative - prints number
+ * @i: The number to print
  */
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%i is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%i is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%i is negative\n", n);
-	}
-
-	return (0);
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is zero\n", i);
 }
